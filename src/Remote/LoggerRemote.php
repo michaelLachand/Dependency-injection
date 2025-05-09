3c345f68329c18c3e3bcadc1caf1b9c83df0237d
Michael Lachand <lachand_michael@hotmail.com>
2025-05-09 14:42:12 +0200

    Add Target attribute for logger

--- src/Remote/LoggerRemote.php
+++ src/Remote/LoggerRemote.php
@@ -4,11 +4,13 @@ namespace App\Remote;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 
 #[AsDecorator(ButtonRemote::class)]
 final class LoggerRemote implements RemoteInterface
 {
     public function __construct(
+        #[Target('buttonsLogger')]
         private LoggerInterface $logger,
         private RemoteInterface $inner,
     ) {
